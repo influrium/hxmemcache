02ed7113483d3e3079310564c37ce92ff0947e76
ConstNW <constnw@gmail.com>
2020-01-08 00:11:30 +0300

    fix tests
    turn off tests of PooledClient on targets that not support threads

--- test/Main.hx
+++ test/Main.hx
@@ -4,8 +4,9 @@ import utest.ui.Report;
 
 import cases.*;
 import hxmemcache.Client;
+#if target.threaded
 import hxmemcache.PooledClient;
-
+#end
 
 class Main
 {
@@ -50,14 +51,17 @@ class Main
         runner.addCase(new PrefixedClientTest());
         runner.addCase(new SerdeTest());
         runner.addCase(new ErrorTest());
+#if target.threaded
         runner.addCase(new PooledClientTest());
         runner.addCase(new PrefixedPooledClientTest());
-
+#end
         Report.create(runner);
         runner.run();
     }
 
     public static function makeClient( ?options : ClientOptions ) return new Client(params.host, params.port, options);
-    public static function makePooledClient( ?options : PooledClientOptions ) return new PooledClient(params.host, params.port, options);
     public static function makeCustomizedClient( ?options : ClientOptions ) return new mock.CustomizedClient(params.host, params.port, options);
+#if target.threaded
+    public static function makePooledClient( ?options : PooledClientOptions ) return new PooledClient(params.host, params.port, options);
+#end
 }
